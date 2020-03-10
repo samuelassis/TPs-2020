6c99871723b5343faf7f610506ff75159d42b851
samuel_assis <samuelassisvieira@gmail.com>
2020-03-10 10:38:44 -0300

    main

--- Matematica Discreta/relacao.c
+++ Matematica Discreta/relacao.c
@@ -1,16 +1,33 @@
 #include "stdio.h"
-
+#include "stdlib.h"
 typedef struct Pair{
 	int x;
 	int y;
 }Pair;
 
 void main(){
-	int n_elements, i;
-
-	scanf("%d", &n_elements);
-	for(i=0;i < n_elements; i++){
-		
+	int n_set, bigger, value, a, b, i,j;
+	int **matrix
+	bigger = 0;
+	
+	scanf("%d", &n_set);
+	for(i=0; i<n_set;i++){
+		scanf("%d",&value);
+		if(value > bigger){
+			bigger = value;
+		}
+	}
+	matrix = (int **) malloc(bigger+1 * sizeof(int*));
+	for(i=0;i<bigger+1;i++){
+		matrix[i] = (int*) malloc(bigger+1 * sizeof(int));
+		for(j=0; j < bigger+1; j++){
+			matrix[i][j] = 0;
+		}
 	}
 
+	while(scanf("%d %d", &a, &b)=! EOF){
+		matrix[a][b] = 1;
+	};
+
+	free(matrix);
 }
