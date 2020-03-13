1bce26ec67b3bffd13705d64667f81dd0869bae9
samuel_assis <samuelassisvieira@gmail.com>
2020-03-13 14:12:13 -0300

    Funções

--- Matematica Discreta/relacao.c
+++ Matematica Discreta/relacao.c
@@ -5,9 +5,48 @@ typedef struct Pair{
 	int y;
 }Pair;
 
+void printMatrix(int** matrix,int lin,int col){
+	int i, j;
+	for(i=1;i<lin;i++){
+		for(j=1;j<col;j++){
+			printf("%d ",matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int reflexive(int ** matrix, int size){
+	int i, boo;
+	boo = 1;
+	for(i=1; i<size;i++){
+		if(matrix[i][i] == 0)
+			boo = 0;
+	}
+	return boo;
+}
+
+int symmetric(int**matriz, int size){
+	int i, j, boo;
+	boo = 1;
+	for(i=1;i<size;i++){
+		for(j=1;j<size;j++){
+			if(matrix[i][j] == 1){
+				if(matrix[j][i] == 0){
+					boo = 0;
+				}
+			}
+		}
+	}
+	return boo;
+}
+
+int asymmetric(int **matrix, int size){
+	
+}
+
 void main(){
 	int n_set, bigger, value, a, b, i,j;
-	int **matrix
+	int **matrix;
 	bigger = 0;
 	
 	scanf("%d", &n_set);
@@ -17,17 +56,19 @@ void main(){
 			bigger = value;
 		}
 	}
-	matrix = (int **) malloc(bigger+1 * sizeof(int*));
+	bigger = bigger+1;
+	matrix = (int **) malloc(bigger * sizeof(int));
 	for(i=0;i<bigger+1;i++){
-		matrix[i] = (int*) malloc(bigger+1 * sizeof(int));
+		matrix[i] = (int*) malloc(bigger * sizeof(int));
 		for(j=0; j < bigger+1; j++){
 			matrix[i][j] = 0;
 		}
 	}
 
-	while(scanf("%d %d", &a, &b)=! EOF){
-		matrix[a][b] = 1;
+	while(scanf("%d %d", &a, &b) != EOF){
+		if((a-b)%2 == 0)	
+			matrix[a][b] = 1;
 	};
-
+	printMatrix(matrix,bigger,bigger);
 	free(matrix);
 }
